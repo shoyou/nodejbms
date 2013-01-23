d1b941853d54e1ab8473a7f6f7430101f1007d11
shoyou <jiangyong@jetsen.cn>
2013-01-23 23:33:43 +0800

    Update models/role.js

--- models/role.js
+++ models/role.js
@@ -3,11 +3,15 @@
  *  @author: JY
  *  @since: 2013-01-22
  */
+require('./department');
 var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
+var Department = mongoose.model('Department');
 
 var RoleSchema = new Schema({
     rolename: String,
+    roletype: String,
+    dept: {type: Schema.Types.ObjectId, ref: 'Department'},
     remark: String,
     locked: {type: Number, default: 0},
     create_date: {type: Date, default: Date.now},
